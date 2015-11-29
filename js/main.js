dd4d45f79145fb336df2bc11fcf98bdaa1c22da4
pamtabak <pamelatabak@poli.ufrj.br>
2015-11-29 16:23:12 -0200

    cleaned code

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ function init(){
  	scene = new THREE.Scene(); // set up the scene
 
  	camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
- 	// camera = new THREE.PerspectiveCamera (75, window.innerWidth / window.innerHeight, 0.1, 1000); // set up the camera. Parameters: field of view, aspect ratio, near, far
  	
  	// Set up the rendered
  	renderer = new THREE.WebGLRenderer();
@@ -18,23 +17,25 @@ function init(){
 	renderer.setSize(window.innerWidth, window.innerHeight); // set the size at which we want it to render our app
 	document.body.appendChild(renderer.domElement);
 	
-	raycaster = new THREE.Raycaster();
+	raycaster = new THREE.Raycaster(); // initializing raycaster
 
  	// Ilumination
  	var light = new THREE.DirectionalLight( 0xffffff, 1 );
 	light.position.set( 1, 1, 1 ).normalize();
 	scene.add( light );
 
-	// Draw Cube
+	// Draw Initial Cube
 	var geometry = new THREE.BoxGeometry( 100, 100, 100 ); // object that contains all the points (vertices) and fill (faces) of the cube. 
 	var material = new THREE.MeshBasicMaterial( { color: 0xff56c0 } );
 	cube = new THREE.Mesh( geometry, material );
 	
-	scene.add( cube ); // By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). This would cause both the camera and the cube to be inside each other. To avoid this, we simply move the camera out a bit.
+	scene.add( cube ); 
+	// By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). 
+	// This would cause both the camera and the cube to be inside each other. 
+	// To avoid this, we simply move the camera out a bit.
 	camera.position.z = 500;
 
-	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-				document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+	document.addEventListener( 'mousedown' , onMouseLeftButtonDown, false );
 
 	window.addEventListener( 'resize', onWindowResize, false );
 }
@@ -71,10 +72,9 @@ function createNewCubes(){
 	console.log("drawing new cube");
 	var geometry = new THREE.BoxGeometry( 100, 100, 100 ); // object that contains all the points (vertices) and fill (faces) of the cube. 
 	var material = new THREE.MeshBasicMaterial( { color: Math.random() * 0xff56c0 } ); // selecting cube color
-	cube = new THREE.Mesh( geometry, material ); // initializing cube
+	cube         = new THREE.Mesh( geometry, material ); // initializing cube
 
 	var position = new THREE.Vector3((x/window.innerWidth)*2 -1,-(y/window.innerHeight)*2 +1, 0); // creating vector with cube`s coordinates
-	//var position = new THREE.Vector3(x,y, 1); // creating vector with cube`s coordinates
 	position.unproject( camera ); // projects camera on vector plan
 	
 	var dir            = position.sub(camera.position).normalize();
@@ -82,20 +82,10 @@ function createNewCubes(){
     var cameraPosition = camera.position.clone().add(dir.multiplyScalar(distance));
 
 	cube.position.set(cameraPosition.x,cameraPosition.y,cameraPosition.z);
-	scene.add( cube ); // By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). This would cause both the camera and the cube to be inside each other. To avoid this, we simply move the camera out a bit.
-		
+	scene.add( cube ); 
 }
 
-function onDocumentTouchStart( event ) {
-	event.preventDefault();
-				
-	event.clientX = event.touches[0].clientX;
-	event.clientY = event.touches[0].clientY;
-	onDocumentMouseDown( event );
-
-}
-
-function onDocumentMouseDown( event ) {
+function onMouseLeftButtonDown ( event ) {
 	console.log("left mouse button clicked");
 	event.preventDefault();
 
